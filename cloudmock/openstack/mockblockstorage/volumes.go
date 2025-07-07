efb4831ffb717c69ad40209c2c0a00f43fc5a12a
Robert Hoffmann <roho@uhurutec.com>
2025-07-07 14:12:57 +0200

    Add json tags

--- cloudmock/openstack/mockblockstorage/volumes.go
+++ cloudmock/openstack/mockblockstorage/volumes.go
@@ -38,8 +38,8 @@ type volumeGetResponse struct {
 }
 
 type volumeMocks struct {
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type volumeCreateRequest struct {
